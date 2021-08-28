3a853dbd554126b0c1369eb632f32fc15ce3faa5
mrhpn <mr.htetphyonaing@gmail.com>
2021-08-29 01:04:38 +0630

    Add project files.

---
+++ YuuJin/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using Windows.ApplicationModel.Activation;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+using YuuJin.Services;
+
+namespace YuuJin
+{
+    public sealed partial class App : Application
+    {
+        private Lazy<ActivationService> _activationService;
+
+        private ActivationService ActivationService
+        {
+            get { return _activationService.Value; }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            UnhandledException += OnAppUnhandledException;
+
+            // Deferred execution until used. Check https://docs.microsoft.com/dotnet/api/system.lazy-1 for further info on Lazy<T> class.
+            _activationService = new Lazy<ActivationService>(CreateActivationService);
+        }
+
+        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            if (!args.PrelaunchActivated)
+            {
+                await ActivationService.ActivateAsync(args);
+            }
+
+            // entends views to the title bar
+            var coreTitleBar = Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        protected override async void OnActivated(IActivatedEventArgs args)
+        {
+            await ActivationService.ActivateAsync(args);
+        }
+
+        private void OnAppUnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            // TODO WTS: Please log and handle the exception as appropriate to your scenario
+            // For more info see https://docs.microsoft.com/uwp/api/windows.ui.xaml.application.unhandledexception
+        }
+
+        private ActivationService CreateActivationService()
+        {
+            return new ActivationService(this, typeof(Views.VocabularyPage), new Lazy<UIElement>(CreateShell));
+        }
+
+        private UIElement CreateShell()
+        {
+            return new Views.ShellPage();
+        }
+    }
+}
---
+++ YuuJin/Views/VocabularyPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Windows.UI.Xaml.Controls;
+
+namespace YuuJin.Views
+{
+    public sealed partial class VocabularyPage : Page, INotifyPropertyChanged
+    {
+        public VocabularyPage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
+
+        private void Add_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectionChanged_Level(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedId = ((ComboBoxItem)e.AddedItems[0]).Tag.ToString();
+
+            ComboBox_Unit.IsEnabled = true;
+            ComboBox_Unit.Items.Clear();
+
+            if (selectedId == "2")
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    ComboBox_Unit.Items.Add(new ComboBoxItem { Tag = i, Content = i.ToString() });
+                }
+            }
+            else if (selectedId == "3")
+            {
+                for (int i = 1; i <= 12; i++)
+                {
+                    ComboBox_Unit.Items.Add(new ComboBoxItem { Tag = i, Content = i.ToString() });
+                }
+            }
+            else if (selectedId == "4")
+            {
+                for (int i = 26; i <= 50; i++)
+                {
+                    ComboBox_Unit.Items.Add(new ComboBoxItem { Tag = i, Content = i.ToString() });
+                }
+            }
+            else if (selectedId == "5")
+            {
+                for (int i = 1; i <= 25; i++)
+                {
+                    ComboBox_Unit.Items.Add(new ComboBoxItem { Tag = i, Content = i.ToString() });
+                }
+            }
+        }
+    }
+}
